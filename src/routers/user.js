f1cbf0dd8a145abc70219d5bd9769c1f099db688
ranbm <ran.ben.melech1@gmail.com>
2021-07-18 19:11:49 +0300

    1

--- src/routers/user.js
+++ src/routers/user.js
@@ -30,6 +30,7 @@ router.post('/users', async (req, res) => {
         const token = await user.generateAuthToken()
         res.status(201).send({ user, token })
     } catch (e) {
+        console.log(e.message)
         res.status(400).send(e)
     }
 })
