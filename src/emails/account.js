9d575662a7c404621aa23f72b79865cef1db6b56
ranbm <ran.ben.melech1@gmail.com>
2021-07-18 16:35:28 +0300

    new init

---
+++ src/emails/account.js
@@ -0,0 +1,23 @@
+const sgMail=require('@sendgrid/mail')
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const sendWelcomeEmail=(email,name)=>{
+    sgMail.send({
+        to: email,
+        from:'ran.ben.melech1@gmail.com',
+        subject: 'Welcome to Who Can Help',
+        text:'Thanks for joining in! ,Let us know how can we help'
+    })
+}
+const sendGoodByeEmail=(email,name)=>{
+    sgMail.send({
+        to: email,
+        from:'ran.ben.melech1@gmail.com',
+        subject: "Wer'e sad to see you leave",
+        text:`dont gooooo, ${name}.`
+    })
+}
+module.exports=({
+    sendWelcomeEmail,
+    sendGoodByeEmail  
+})
